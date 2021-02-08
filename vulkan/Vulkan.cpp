47213ed1aad574d54cc8ff00d12138613149b70a
Calvin Ruiz <calvin.ruiz@epitech.eu>
2021-02-08 09:57:01 +0100

    Don't use SRGB NON_LINEAR

--- vulkan/Vulkan.cpp
+++ vulkan/Vulkan.cpp
@@ -356,7 +356,7 @@ void Vulkan::createDescriptorSets()
 static VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
 
     for (const auto& availableFormat : availableFormats) {
-        if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+        if (availableFormat.format == VK_FORMAT_B8G8R8A8_UNORM) {
             return availableFormat;
         }
     }
